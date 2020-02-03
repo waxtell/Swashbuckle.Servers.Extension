3009b86b8ee562bcf2acf23c6b84e110dc3c761c
Will Axtell <waxtell@transtar1.com>
2020-02-03 12:50:52 -0500

    Initial commit

---
+++ Swashbuckle.Servers.Extension/Extensions/SwaggerGenOptionsExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+// ReSharper disable once CheckNamespace
+namespace Swashbuckle.Servers.Extension
+{
+    public static class SwaggerGenOptionsExtensions
+    {
+        public static SwaggerGenOptions WithServers(this SwaggerGenOptions swaggerGenOptions, Action<ServersOptions> setupAction)
+        {
+            var options = new ServersOptions();
+
+            setupAction.Invoke(options);
+
+            swaggerGenOptions.DocumentFilter<ServersDocumentFilter>(options);
+
+            return swaggerGenOptions;
+        }
+
+        public static SwaggerGenOptions WithServers(this SwaggerGenOptions swaggerGenOptions, IEnumerable<OpenApiServer> servers)
+        {
+            var options = new ServersOptions { Servers = servers };
+      
+            swaggerGenOptions.DocumentFilter<ServersDocumentFilter>(options);
+
+            return swaggerGenOptions;
+        }
+    }
+}
---
+++ Swashbuckle.Servers.Extension/Options/ServersOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+
+// ReSharper disable once CheckNamespace
+namespace Swashbuckle.Servers.Extension
+{
+    public class ServersOptions
+    {
+        public IEnumerable<OpenApiServer> Servers { get; set; }
+    }
+}
